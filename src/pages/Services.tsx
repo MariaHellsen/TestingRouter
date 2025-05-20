4833ba5ea2ff44c7ac6f787e93b51d1416e80d80
MariaHellsen <maria.hellsen@outlook.com>
2025-05-20 16:52:03 +0200

    Added small styling to header and tried useParams

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router";
+import { useParams } from "react-router";
 
 export const Services = () => {
+  const { name } = useParams();
+
   return (
     <>
       <div className="servies">
-        <h1>Our Services</h1>
+        <h1>Our Services: {name}</h1>
         <ul>
           <li>
             <Link to={"/integration"}>Integrations</Link>
